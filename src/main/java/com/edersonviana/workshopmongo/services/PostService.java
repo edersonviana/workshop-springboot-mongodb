b0a6b9be3689a429e44e44b163f1837106045a30
Ederson Viana <edersonvangeli@gmail.com>
2025-01-03 16:35:34 -0300

    Consulta simples com @Query

--- src/main/java/com/edersonviana/workshopmongo/services/PostService.java
+++ src/main/java/com/edersonviana/workshopmongo/services/PostService.java
@@ -22,7 +22,7 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text){
-		return postRepository.findByTitleContainingIgnoringCase(text);
+		return postRepository.searchTitle(text);
 	}
 
 }
